7d87d7798e9cb6ede86ea861db81886000042959
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-11 13:43:30 +0100

    creato i box

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+let container = document.querySelector(".container");
+
+for(let i=1; i<=100; i++){
+let newBox = document.createElement("div");
+newBox.classList.add("box");
+container.appendChild(newBox);
+}
+
 
 
