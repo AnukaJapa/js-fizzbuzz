979c525d8528edd5510ee3bb97aa23ad70b0df59
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-11 14:45:01 +0100

    fatto la Logica

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ for(let i=1; i<=100; i++){
 let newBox = document.createElement("div");
 newBox.classList.add("box");
 container.appendChild(newBox);
+
+let boxWord ="";
+if(i%3==0){
+boxWord +="Fizz"
+if(i%5==0) boxWord +="Buzz"
+}else if(i%5==0) {
+    boxWord +="Buzz";
+} else {
+    boxWord = i; 
+}
+newBox.textContent = boxWord;
+console.log(boxWord);
 }
 
 
